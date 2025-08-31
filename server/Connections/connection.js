c6c471b257e9125adb28fe5aa48213fdcddd0c62
akhilesh <akhilehs@gmail.com>
2025-08-31 14:22:58 +0530

    yes

--- server/Connections/connection.js
+++ server/Connections/connection.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
+    await mongoose.connect(process.env.Mongo_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     console.log("✅ MongoDB Connected Successfully");
